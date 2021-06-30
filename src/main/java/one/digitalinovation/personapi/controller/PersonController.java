56c36051ba783351e3cf3581db7c2bcbd1090f1a
edupedro <edu.pedro.andrade@gmail.com>
2021-06-30 09:33:06 -0300

    last commit before given project

--- src/main/java/one/digitalinovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinovation/personapi/controller/PersonController.java
@@ -1,14 +1,13 @@
 package one.digitalinovation.personapi.controller;
 
 import lombok.AllArgsConstructor;
+import one.digitalinovation.personapi.dto.request.PersonDTO;
+import one.digitalinovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinovation.personapi.exception.PersonNotFoundException;
 import one.digitalinovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import one.digitalinovation.personapi.dto.response.MessageResponseDTO;
-import one.digitalinovation.personapi.dto.request.PersonDTO;
 
 import javax.validation.Valid;
 import java.util.List;
